e0cc0d34d79cc4274804b51e5c8961f0298fe186
Michael <michael_makam18@hotmail.com>
2020-01-14 20:00:38 +0000

    Add sfas project

---
+++ GradsInGames-sfasx/Assets/Scripts/Character.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] private float SingleNodeMoveTime = 0.5f;           //controls movement speed of
+
+    public EnvironmentTile CurrentPosition { get; set; }
+
+    private IEnumerator DoMove(Vector3 position, Vector3 destination)
+    {
+        // Move between the two specified positions over the specified amount of time
+        if (position != destination)
+        {
+            transform.rotation = Quaternion.LookRotation(destination - position, Vector3.up);
+
+            Vector3 transformPos = transform.position;
+            float time = 0.0f;
+
+            while (time < SingleNodeMoveTime)
+            {
+                time += Time.deltaTime;
+                transformPos = Vector3.Lerp(position, destination, time / SingleNodeMoveTime);
+                transform.position = transformPos;
+                yield return null;
+            }
+        }
+    }
+
+    private IEnumerator DoGoTo(List<EnvironmentTile> route)
+    {
+        // Move through each tile in the given route
+        if (route != null)
+        {
+            //get the position of the current tile
+            Vector3 position = CurrentPosition.Position;
+            for (int count = 0; count < route.Count; ++count)
+            {
+                Vector3 next = route[count].Position;
+                yield return DoMove(position, next);
+                CurrentPosition = route[count];
+                position = next;
+            }
+        }
+    }
+
+    public void GoTo(List<EnvironmentTile> route)
+    {
+        // Clear all coroutines before starting the new route so 
+        // that clicks can interupt any current route animation
+        StopAllCoroutines();
+        StartCoroutine(DoGoTo(route));
+    }
+}
